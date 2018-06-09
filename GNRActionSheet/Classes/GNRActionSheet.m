314f7a6bea96aa048b68dc8a96f111fbf78b8867
Luca <ly918@qq.com>
2018-06-09 17:46:20 +0800

    优化

--- GNRActionSheet/Classes/GNRActionSheet.m
+++ GNRActionSheet/Classes/GNRActionSheet.m
@@ -9,30 +9,7 @@
 #import "GNRActionSheetCell.h"
 #import <Masonry/Masonry.h>
 #import <GNRFoundation/UIView+GNRSafeArea.h>
-
-@interface UIView (LSCore)
-
-#pragma mark - 设置部分圆角
-/**
- *  设置部分圆角(绝对布局)
- *
- *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
- *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
- */
-- (void)addRoundedCorners:(UIRectCorner)corners
-withRadii:(CGSize)radii;
-/**
- *  设置部分圆角(相对布局)
- *
- *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
- *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
- *  @param rect    需要设置的圆角view的rect
- */
-- (void)addRoundedCorners:(UIRectCorner)corners
-withRadii:(CGSize)radii
-viewRect:(CGRect)rect;
-
-@end
+#import <GNRFoundation/UIView+Factory.h>
 
 @interface GNRActionSheet ()<UITableViewDelegate,UITableViewDataSource>
 @property (nonatomic,assign)CGFloat contentTotalHeight;
@@ -119,7 +96,7 @@ viewRect:(CGRect)rect;
     [self.view layoutIfNeeded];
     
     self.tableView.scrollEnabled = self.contentTotalHeight>CGRectGetHeight(self.view.bounds)*0.8;
-    [self.tableView addRoundedCorners:UIRectCornerTopLeft|UIRectCornerTopRight withRadii:CGSizeMake(6.0, 6.0) viewRect:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), self.contentTotalHeight)];
+    [self.tableView addRoundedCorners:UIRectCornerTopLeft|UIRectCornerTopRight radii:CGSizeMake(6.0, 6.0) viewRect:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), self.contentTotalHeight)];
 }
 
 - (void)viewWillAppear:(BOOL)animated{
@@ -262,41 +239,4 @@ viewRect:(CGRect)rect;
     [super didReceiveMemoryWarning];
 }
 
-
-@end
-
-@implementation UIView (LSCore)
-#pragma mark - 设置部分圆角
-/**
- *  设置部分圆角(绝对布局)
- *
- *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
- *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
- */
-- (void)addRoundedCorners:(UIRectCorner)corners
-                withRadii:(CGSize)radii {
-    
-    UIBezierPath* rounded = [UIBezierPath bezierPathWithRoundedRect:self.bounds byRoundingCorners:corners cornerRadii:radii];
-    CAShapeLayer* shape = [[CAShapeLayer alloc] init];
-    [shape setPath:rounded.CGPath];
-    
-    self.layer.mask = shape;
-}
-
-/**
- *  设置部分圆角(相对布局)
- *
- *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
- *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
- *  @param rect    需要设置的圆角view的rect
- */
-- (void)addRoundedCorners:(UIRectCorner)corners
-                withRadii:(CGSize)radii
-                 viewRect:(CGRect)rect {
-    
-    UIBezierPath* rounded = [UIBezierPath bezierPathWithRoundedRect:rect byRoundingCorners:corners cornerRadii:radii];
-    CAShapeLayer* shape = [[CAShapeLayer alloc] init];
-    [shape setPath:rounded.CGPath];
-    self.layer.mask = shape;
-}
 @end
